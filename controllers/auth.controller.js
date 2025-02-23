6293ca805039e86da2551e59f3692f75ce589839
victor <oguntayovictor65@gmail.com>
2025-02-23 16:50:19 +0100

    add loginUser controller function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,9 @@
 const { StatusCodes } = require("http-status-codes");
-const { CustomAPIError, BadRequestError } = require("../errors");
+const {
+  CustomAPIError,
+  BadRequestError,
+  UnauthenticatedError,
+} = require("../errors");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const { createJWT } = require("../utils");
@@ -22,7 +26,28 @@ const registerUser = async (req, res) => {
   res.status(StatusCodes.CREATED).json(user);
 };
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide both value");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new BadRequestError("Invalid Credentials");
+  }
+  const correctPassword = user.comparePassword(password);
+  if (!correctPassword) {
+    throw new UnauthenticatedError("Incorrect password");
+  }
+  const tokenUser = {
+    name: user.name,
+    email: user.email,
+    userId: user._id,
+    role:user.role
+  };
+  attachCookieToResponse(res, tokenUser);
+  res.status(StatusCodes.OK).json(tokenUser);
+};
 
 const logoutUser = async (req, res) => {};
 
