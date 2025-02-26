76b9be5e7048ae4ad501b6df3c8cf54020bbd074
victor <oguntayovictor65@gmail.com>
2025-02-26 14:55:44 +0100

    work on createProduct controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,14 @@
-const getAllProduct = async (rreq, res) => {
+const { StatusCodes } = require('http-status-codes');
+const Product = require('../routes/product.route')
+
+
+const getAllProduct = async (req, res) => {
   res.send("get all product");
 };
-const createProduct = async (rreq, res) => {
+const createProduct = async (req, res) => {
+    req.body.user = req.user.userId
+    const product = await Product.create(req.body)
+    res.status(StatusCodes.CREATED).json({product})
   res.send("get all product");
 };
 const getSingleProduct = async (req, res) => {
