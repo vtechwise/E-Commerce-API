18ce409a6f1ae80186938809dc3eb5e87fb71ff4
victor <oguntayovictor65@gmail.com>
2025-02-26 14:39:51 +0100

    add authenticateUser and authorizePermissions to some route in productRoute

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,28 @@
-const express = require('express')
-const { getAllProduct, createProduct, getSingleProduct, deleteProduct, updateProduct, uploadImage } = require('../controllers/product.controller')
+const express = require("express");
+const {
+  getAllProduct,
+  createProduct,
+  getSingleProduct,
+  deleteProduct,
+  updateProduct,
+  uploadImage,
+} = require("../controllers/product.controller");
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').get(getAllProduct).post(createProduct)
-router.post('/upload', uploadImage)
-router.route('/:id').get(getSingleProduct).delete(deleteProduct).patch(updateProduct)
+router
+  .route("/")
+  .get(getAllProduct)
+  .post([authenticateUser, authorizePermissions("admin")], createProduct);
+router.post("/upload", uploadImage);
+router
+  .route("/:id")
+  .get(getSingleProduct)
+  .delete([authenticateUser, authorizePermissions("admin")], deleteProduct)
+  .patch([authenticateUser, authorizePermissions("admin")], updateProduct);
 
-
-module.exports = router 
-
+module.exports = router;
