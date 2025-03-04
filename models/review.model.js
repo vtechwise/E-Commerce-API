c74961ed44ee4c7f25b22884a6a3eecfac7c34d5
victor <oguntayovictor65@gmail.com>
2025-03-04 23:04:19 +0100

    add reviewSchema and reviewModel

---
+++ models/review.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: [true, "Please provide rating"],
+  },
+  title: {
+    type: String,
+    maxlength: 100,
+    trim: true,
+    required: [true, "Please provide review title"],
+  },
+  comment: {
+    type: String,
+    required: [true, "Please provide review comment "],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    required:true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+    required:true
+  },
+});
+
+ReviewSchema.index({product:1, user:1}, {unique:true})
+
+module.exports = mongoose.model("Review", ReviewSchema);
