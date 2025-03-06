ff4b6e815b8e82532771533ec5972ec9173c36bb
victor <oguntayovictor65@gmail.com>
2025-03-06 07:39:32 +0100

    add logic for caculating averageRating and numOfReview

--- models/review.model.js
+++ models/review.model.js
@@ -19,17 +19,46 @@ const ReviewSchema = new mongoose.Schema({
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-    required:true,
+    ref: "user",
+    required: true,
   },
   product: {
     type: mongoose.Schema.Types.ObjectId,
-      ref: "product",
-    required:true
+    ref: "product",
+    required: true,
   },
-  
 });
 
-ReviewSchema.index({product:1, user:1}, {unique:true})
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: null,
+        averageRating: { $avg: "rating" },
+        numOfReview: { $sum: 1 },
+      },
+    },
+  ]);
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating) || 0,
+        numOfReview: result[0]?.numOfReview || 0,
+      }
+    );
+  } catch (error) {}
+};
+
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
+ReviewSchema.post("remove", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
 
 module.exports = mongoose.model("Review", ReviewSchema);
