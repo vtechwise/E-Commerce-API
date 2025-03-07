40f5a98f6572e85ab70e7e62a77f9b304c9b3ff8
victor <oguntayovictor65@gmail.com>
2025-03-07 17:38:29 +0100

    fix calculateAveragerating bug in reviewModel

--- models/review.model.js
+++ models/review.model.js
@@ -32,24 +32,23 @@ const ReviewSchema = new mongoose.Schema({
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 ReviewSchema.statics.calculateAverageRating = async function (productId) {
-  const result = this.aggregate([
+  const result =await  this.aggregate([
     { $match: { product: productId } },
     {
       $group: {
         _id: null,
-        averageRating: { $avg: "rating" },
+        averageRating: { $avg: "$rating" },
         numOfReview: { $sum: 1 },
       },
     },
   ]);
-  console.log(result);
 
   try {
     await this.model("Product").findOneAndUpdate(
       { _id: productId },
       {
         averageRating: Math.ceil(result[0]?.averageRating) || 0,
-        numOfReview: result[0]?.numOfReview || 0,
+        numOfReviews: result[0]?.numOfReview || 0,
       }
     );
   } catch (error) {}
